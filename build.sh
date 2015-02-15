186cf420d4ffc0f9b1e2b30814042dd64dc7f618
Ben Taylor <ben@hfx1084>
2015-02-15 19:46:44 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-#download urbanness data from the government!
-mkdir urbannessdata
-cd urbannessdata
+#download urbanness data from the government
+#https://www.gov.uk/government/statistical-data-sets/local-enterprise-partnerships-leps-rural-urban-gis-shapefiles
+mkdir -p urbannessdata && cd urbannessdata
 ../downloadurbandata.sh
 for z in *.zip; do unzip $z; done
 ../importurbannessdata.sh
+cd ..
 
-#download country data
-wget http://download.geofabrik.de/europe/great-britain-latest.osm.pbf
-wget http://download.geofabrik.de/europe/france-latest.osm.pbf
+#download scottish urbanness data:
+mkdir -p urbannessdata_scotland && cd urbannessdata_scotland
+curl http://www.scotland.gov.uk/Resource/0039/00399160.zip>scotland.zip
+unzip scotland.zip
+shp2pgsql -s 27700 SGUR_2011_2012_HWM.shp scotland | psql osm
+psql osm -c "insert into urbanness(ru_def_des, geom2) select 'U_CT', ST_Transform(geom, 900913) from scotland where ur6fold = 1"
+
