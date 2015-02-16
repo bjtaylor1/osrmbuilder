36f95f9837d9de316fbd0b49e5d79cb67c8dee73
Ubuntu <ubuntu@ip-172-31-42-156.us-west-2.compute.internal>
2015-02-16 14:29:19 +0000

    corrected typo in refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,7 +4,7 @@ set -e
 
 
 #download country data
-curl "http://download.geofabrik.de/europe/great-britain-latest.osm.pbf">great-britan-latest.osm.pbf
+curl "http://download.geofabrik.de/europe/great-britain-latest.osm.pbf">great-britain-latest.osm.pbf
 curl "http://download.geofabrik.de/europe/france-latest.osm.pbf">france-latest.osm.pbf
 
 #import country data
