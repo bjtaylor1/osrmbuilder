d89618ebcc07d53d501cded63b571cf94657ca4d
Ben Taylor <ben@hfx1084>
2015-02-16 13:21:24 +0000

    added database flag to imposm call

--- refresh.sh
+++ refresh.sh
@@ -8,7 +8,7 @@ curl "http://download.geofabrik.de/europe/great-britain-latest.osm.pbf">great-br
 curl "http://download.geofabrik.de/europe/france-latest.osm.pbf">france-latest.osm.pbf
 
 #import country data
-imposm --read --write --optimize --deploy-production-tables great-britain-latest.osm.pbf
+imposm --read --write --optimize --deploy-production-tables great-britain-latest.osm.pbf -d osm
 
 #combine
 osmosis --read-pbf file=great-britain-latest.osm.pbf --read-pbf file=france-latest.osm.pbf --merge --write-pbf file=countries.osm.pbf
