5dcfc580f4b3ab806fff8232fa11eee2fa05a7c9
Ubuntu <ubuntu@ip-172-31-34-42.us-west-2.compute.internal>
2015-02-17 11:00:56 +0000

    updated buildosrm to merge mine

--- buildosrm.sh
+++ buildosrm.sh
@@ -5,6 +5,9 @@ rm -rf Project-OSRM
 git clone https://github.com/Project-OSRM/osrm-backend Project-OSRM --branch master
 
 cd Project-OSRM
+git remote add mine https://github.com/bjtaylor1/osrm-backend
+git pull mine master
+
 rm -f CMakeCache.txt
 rm -rf build
 mkdir build
