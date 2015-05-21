f1aea2338a1534ac10a5bd5a027ef0647de22a7d
Ben <bentaylortheonly@gmail.com>
2015-05-21 22:18:15 +0100

    two step merge

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ set -e
 ./refreshsourcedata.sh ireland-and-northern-ireland-latest
 
 #merge britain and france together
-osmosis --read-pbf file=great-britain-latest.osm.pbf --read-pbf file=france-latest.osm.pbf --read-pbf file=ireland-and-northern-ireland-latest.osm.pbf \
-					--merge --write-pbf file=countries.osm.pbf
+osmosis --read-pbf file=great-britain-latest.osm.pbf --read-pbf file=ireland-and-northern-ireland-latest.osm.pbf --merge --write-pbf file=uk.osm.pbf
+osmosis --read-pbf file=uk.osm.pbf --read-pbf file=france-latest.osm.pbf --merge --write-pbf file=countries.osm.pbf
 
 #build osrm
 ./buildosrm.sh
