9a967c9224b77f4ba4bfb17e4fcda3155b8d5ae7
Ubuntu <ubuntu@ip-172-31-33-223.us-west-2.compute.internal>
2015-05-24 20:37:54 +0000

    refreshsourcedata moves updated over original

--- refreshsourcedata.sh
+++ refreshsourcedata.sh
@@ -7,6 +7,7 @@ else
 	if [[ -n $(find $1.osm.pbf -mtime -30 -size +10M) ]]; then
 		echo "$1 exists and is worth patching (less than 30 days old and over 10MB)  - updating"
 		osmupdate $1.osm.pbf $1.updated.osm.pbf --day --verbose 1
+		mv $1.updated.osm.pbf $1.osm.pbf
 	else
 		echo "$1 doesn't exist, is under 10MB or is older than 30 days - redownloading"		
 		curl "http://download.geofabrik.de/europe/$1.osm.pbf">$1.osm.pbf
