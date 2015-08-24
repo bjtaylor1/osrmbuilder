ddb33fae94ecc6ed5bd8832065b60cb2d7fc6fae
Ben <bentaylortheonly@gmail.com>
2015-08-24 13:48:02 +0100

    buildosrm just gets mine (master can be merged in)

--- buildosrm.sh
+++ buildosrm.sh
@@ -2,11 +2,9 @@
 
 cd ..
 rm -rf Project-OSRM
-git clone https://github.com/Project-OSRM/osrm-backend Project-OSRM --branch master
+git clone https://github.com/bjtaylor1/osrm-backend Project-OSRM --branch master
 
 cd Project-OSRM
-git remote add mine https://github.com/bjtaylor1/osrm-backend
-git pull mine master
 
 rm -f CMakeCache.txt
 rm -rf build
