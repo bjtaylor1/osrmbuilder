671e4a1503383be5d1f763e11568407034716ed4
BJTaylor <bentaylortheonly@gmail.com>
2017-11-26 10:24:18 +0000

    Update createurbannessscorestable.sql

--- createurbannessscorestable.sql
+++ createurbannessscorestable.sql
@@ -1,1 +1,5 @@
 select r.osm_id, min(ud.score) as score into urbannessscores from urbanness u join urbanness_defs ud on u.ru_def_des = ud.ru_def_des join osm_roads r on ST_Intersects(r.geometry, u.geom2) group by r.osm_id;
+
+alter table urbannessscores add constraint urbannessscores_pkey primary key (osm_id);
+
+cluster urbannessscores using urbannessscores_pkey;
