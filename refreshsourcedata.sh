5ffb87afd6f674835fb6b9cc0059c5f74198d241
Ben Taylor <bentaylortheonly@gmail.com>
2018-06-26 10:24:39 +0000

    correct refreshsourcedata

--- refreshsourcedata.sh
+++ refreshsourcedata.sh
@@ -2,6 +2,6 @@
 set -ex
 
 curl "http://download.geofabrik.de/europe/$1.md5">$1.md5
-md5sum -c $1.md5 | curl "http://download.geofabrik.de/europe/$1">$1
+md5sum -c $1.md5 | curl "http://download.geofabrik.de/europe/$1.osm.pbf">$1
 
 
