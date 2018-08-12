3e2e0483b7373e945f3562fe7922e527a669a4c5
BJTaylor <bentaylortheonly@gmail.com>
2018-08-12 13:34:53 +0100

    british isles

--- refreshandmergeallsourcedata.sh
+++ refreshandmergeallsourcedata.sh
@@ -2,11 +2,5 @@
 set -e
 
 #refresh the data
-./refreshsourcedata.sh great-britain-latest.osm.pbf
-./refreshsourcedata.sh france-latest.osm.pbf
-./refreshsourcedata.sh ireland-and-northern-ireland-latest.osm.pbf
-
-#merge britain and france together
-osmosis --read-pbf file=great-britain-latest.osm.pbf --read-pbf file=ireland-and-northern-ireland-latest.osm.pbf --merge --write-pbf file=uk.osm.pbf
-osmosis --read-pbf file=uk.osm.pbf --read-pbf file=france-latest.osm.pbf --merge --write-pbf file=countries.osm.pbf
+./refreshsourcedata.sh british-isles-latest.osm.pbf
 
