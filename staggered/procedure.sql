35d9d11e215425052a507ceb33b78c9a524d84b2
Ben Taylor <bentaylortheonly@gmail.com>
2018-09-02 19:20:14 +0000

    procs

--- staggered/procedure.sql
+++ staggered/procedure.sql
@@ -1,32 +1,40 @@
 create or replace function staggered ()
 returns void
 as $$
-declare thecursor cursor for
-select wn.way_id as way_id,
-    w2.ref,
-    count(*) as count
-from ways w 
-join  way_nodes wn on w.id = wn.way_id 
-join nodes n on wn.node_id = n.id
-join way_nodes wn2 on wn.node_id = wn2.node_id 
-join ways w2 on wn2.way_id = w2.id
-where 
-w.junction != 'roundabout' and
-wn.way_id != wn2.way_id and 
-w.ref != w2.ref
-group by wn.way_id, w.junction, w2.ref
-having count(*) > 1;
+declare 
+	thecursor cursor for select way_id, ref from staggeredjunctions where way_id = 173688433;
+	subcursor cursor (p_way_id integer, p_ref varchar(255)) is
+		select distinct n.id, geom
+		from nodes n
+		join way_nodes wn on n.id = wn.node_id
+		join way_nodes wn2 on n.id = wn.node_id
+		join ways w2 on wn2.way_id = w2.id
+		where wn.way_id = p_way_id
+		and w2.ref = p_ref;
 
-declare way_id int;
-declare ref varchar(255);
+
+declare v_way_id int;
+declare v_ref varchar(255);
 
 begin
 	open thecursor;
 	loop
-		fetch thecursor into way_id, ref;
+		fetch thecursor into v_way_id, v_ref;
 		exit when not found;
-		raise notice  'way_id = %', way_id;
-		raise notice 'ref = %', ref;
+
+		raise notice  'way_id = %, ref = %', v_way_id, v_ref;
+		create temp table staggerednodes as 
+		select distinct n.id, geom
+		from nodes n
+		join way_nodes wn on n.id = wn.node_id
+		join way_nodes wn2 on n.id = wn.node_id
+		join ways w2 on wn2.way_id = w2.id
+		where wn.way_id = v_way_id
+		and w2.ref = v_ref;
+
+		if (select count(*) from staggerednodes) = 2 then
+			raise notice 'it has got two rows!';
+		end if;
 		exit;
 	end loop;
 	close thecursor;
--- staggered/process.sql
+++ staggered/process.sql
@@ -9,3 +9,18 @@ alter table ways alter column junction set  not null;
 
 create index idx_ways_ref on ways(ref);
 create index idx_ways_junction on ways(junction);
+
+select wn.way_id as way_id,
+    w2.ref
+into staggeredjunctions
+from ways w
+join  way_nodes wn on w.id = wn.way_id
+join nodes n on wn.node_id = n.id
+join way_nodes wn2 on wn.node_id = wn2.node_id
+join ways w2 on wn2.way_id = w2.id
+where
+w.junction != 'roundabout' and
+wn.way_id != wn2.way_id and
+w.ref != w2.ref
+group by wn.way_id, w.junction, w2.ref
+having count(*) = 2
