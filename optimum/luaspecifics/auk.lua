4d859bc4d1c410b796a568525538d1b60a3166b3
Ben Taylor <bentaylortheonly@gmail.com>
2018-09-04 09:09:46 +0000

    remove ferries

---
+++ optimum/luaspecifics/auk.lua
@@ -0,0 +1,25 @@
+module "auk"
+whitelist_speed = 17
+
+speed_profile = {
+  ["trunk"] = 17,
+  ["trunk_link"] = 17,
+  ["primary"] = 17,
+  ["primary_link"] = 17,
+  ["secondary"] = 17,
+  ["secondary_link"] = 17,
+  ["tertiary"] = 17,
+  ["tertiary_link"] = 17,
+  ["unclassified"] = 17,
+  ["residential"] = 17,
+  ["living_street"] = 17,
+  ["service"] = 17,
+  ["ferry"] = 5,
+  ["shuttle_train"] = 10,
+  ["default"] = 17
+}
+
+function get_specific_speed(way)
+return 0
+end
+
