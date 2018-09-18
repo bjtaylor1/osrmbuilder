5fff1eebe72a2a09b5d010e429064370f891f5bc
Ben Taylor <bentaylortheonly@gmail.com>
2018-09-18 11:28:16 +0000

    add preprocess.sh

---
+++ preprocess.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+dropdb --if-exists osm
+createdb osm
+
+psql -d osm -c "create extension postgis;"
+psql -d osm -c "create extension hstore;"
+
+psql -d osm -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql
+psql -d osm -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql
+psql -d osm -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql
+
+osmosis --read-pbf countries.osm.pbf --log-progress --write-pgsql database=osm user=osm password=osm
+
+# after import, change trunks to highway
+# they have issues with lane_count being available
+# (see logged issue for more info)
+psql -d osm -c update ways set tags = tags || 'highway=>primary'::hstore where tags->'highway' = 'trunk'
+
+psql -d osm -f staggered/process.sql
+
+psql -d osm -c "select staggered();"
+
