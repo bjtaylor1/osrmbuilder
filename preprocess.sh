b5c385918b50dacece57b10ab75fc776aca65a99
BJTaylor <bentaylortheonly@gmail.com>
2018-09-20 12:38:38 +0100

    temporarily disable commands after pgsql import.

--- preprocess.sh
+++ preprocess.sh
@@ -9,16 +9,17 @@ psql -d osm -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql
 psql -d osm -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql
 psql -d osm -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql
 
+osmosis --truncate-pgsql database=osm user=osm password=osm
 osmosis --read-pbf rawdata.osm.pbf --log-progress --write-pgsql database=osm user=osm password=osm
 
 # after import, change trunks to highway
 # they have issues with lane_count being available
 # (see logged issue for more info)
-psql -d osm -c "update ways set tags = tags || 'highway=>primary'::hstore where tags->'highway' = 'trunk'"
+# psql -d osm -c "update ways set tags = tags || 'highway=>primary'::hstore where tags->'highway' = 'trunk'"
 
-psql -d osm -f staggered/procedure.sql
-psql -d osm -f staggered/process.sql
+# psql -d osm -f staggered/procedure.sql
+# psql -d osm -f staggered/process.sql
 
-psql -d osm -c "select staggered();"
+# psql -d osm -c "select staggered();"
 
-osmosis --read-pgsql database=osm user=osm password=osm --dataset-dump --write-pbf countries-processed.osm.pbf
+# osmosis --read-pgsql database=osm user=osm password=osm --dataset-dump --write-pbf countries-processed.osm.pbf
