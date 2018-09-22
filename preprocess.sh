73cc0cb79e089d3d86c400ad7d6a3ce6f99c4b15
Ben Taylor <bentaylortheonly@gmail.com>
2018-09-22 12:31:01 +0000

    preprocess

--- preprocess.sh
+++ preprocess.sh
@@ -18,11 +18,11 @@ osmosis --read-pbf rawdata.osm.pbf --log-progress --write-pgsql database=osm use
 # after import, change trunks to highway
 # they have issues with lane_count being available
 # (see logged issue for more info)
-# psql -d osm -c "update ways set tags = tags || 'highway=>primary'::hstore where tags->'highway' = 'trunk'"
+psql -d osm -c "update ways set tags = tags || 'highway=>primary'::hstore where tags->'highway' = 'trunk'"
 
-# psql -d osm -f staggered/procedure.sql
-# psql -d osm -f staggered/process.sql
+psql -d osm -f staggered/procedure.sql
+psql -d osm -f staggered/process.sql
 
-# psql -d osm -c "select staggered();"
+psql -d osm -c "select staggered();"
 
-# osmosis --read-pgsql database=osm user=osm password=osm --dataset-dump --write-pbf countries-processed.osm.pbf
+osmosis --read-pgsql database=osm user=osm password=osm --dataset-dump --write-pbf countries-processed.osm.pbf
