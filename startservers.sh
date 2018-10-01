77cb625836c83dcf92e9afbfcb940ed0db885a90
Ben Taylor <bentaylortheonly@gmail.com>
2018-10-01 19:11:39 +0000

    update startservers.sh

--- startservers.sh
+++ startservers.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-cd /50g/osrmbuilder/auk
-nohup ./osrm-routed -p 5001 countries.osrm>run.log 2>&1 &
-
-cd /50g/osrmbuilder/touristb
-nohup ./osrm-routed -p 5002 countries.osrm>run.log 2>&1 &
+nohup shortest/osrm-routed -p 5001 shortest/countries.osrm>/dev/null 2>&1 &
+nohup optimum/osrm-routed -p 5002 optimum/countries.osrm>/dev/null 2>&1 &
+nohup quickest/osrm-routed -p 5003 quickest/countries.osrm>/dev/null 2>&1 &
+nohup urban/osrm-routed -p 5004 urban/countries.osrm>/dev/null 2>&1 &
