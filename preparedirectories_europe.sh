57be8e200f6bfb78f2c090549c00006ce1835505
Ben Taylor <bentaylortheonly@gmail.com>
2019-07-23 10:33:33 +0100

    add preparedirectories_europe.sh

---
+++ preparedirectories_europe.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+export OSRM_RASTER_SOURCE=`pwd`/rasters/
+for routetype in shortest; do
+	
+  echo Preparing $routetype
+	rm -rf $routetype
+	mkdir $routetype
+
+	./preparedirectory.sh $routetype
+
+  cd $routetype
+  ln -s ../countries.osm.pbf countries.osm.pbf
+  ./osrm-extract countries.osm.pbf -p $routetype.lua
+  ./osrm-contract countries.osrm  
+  cd ..
+done
+
