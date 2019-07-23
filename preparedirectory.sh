1c8426b7ca50742724cb424de3f3d8703f68e1f6
Ben Taylor <bentaylortheonly@gmail.com>
2019-07-23 11:31:14 +0100

    reference correct dir in preparedirectory.sh

--- preparedirectory.sh
+++ preparedirectory.sh
@@ -4,5 +4,5 @@ routetype=$1
 cp -rv lualib $routetype
 cp -rv luaspecifics $routetype
 cp -v $routetype.lua $routetype
-cp -rv ../osrm-backend/profiles/lib $routetype/
-cp -v ../osrm-backend/build/osrm-* $routetype
+cp -rv ~/osrm-backend/profiles/lib $routetype/
+cp -v ~/osrm-backend/build/osrm-* $routetype
